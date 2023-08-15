d05dba4e70d9daadd838f8cd15704c957379cdd8
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-08-15 20:03:57 -0300

    refactor: adds correct types

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
 import React, {useCallback, useEffect, useState} from 'react';
-
 import {LoginScreen} from './src/screens/LoginScreen';
 import {DashboardScreen} from './src/screens/DashboardScreen';
 
-import auth from '@react-native-firebase/auth';
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
 
 export function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
 
-  const onAuthStateChanged = useCallback((user: any) => {
-    setUser(user);
-  }, []);
+  const onAuthStateChanged = useCallback(
+    (userInfo: FirebaseAuthTypes.User | null) => {
+      setUser(userInfo);
+    },
+    [],
+  );
 
   useEffect(() => {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
+    return subscriber;
   }, [onAuthStateChanged]);
 
   return user ? <DashboardScreen /> : <LoginScreen />;
